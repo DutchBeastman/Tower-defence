dbdb6b81f59de12e0c45d72b35fc2fb5350e22e7
DutchBeastman <Fabianverk@hotmail.com>
2013-12-04 12:05:59 +0100

    Update 3

--- Assets/Scripts/Upgrade.cs
+++ Assets/Scripts/Upgrade.cs
@@ -2,43 +2,39 @@
 using System.Collections;
 
 public class Upgrade : MonoBehaviour {
-	//public
-	public Rect windowRect = new Rect(20, 20, 300, 50);
-	public bool doWindow0 = false;
-	public bool doWindow1 = false;
-	public Rect windowRect0 = new Rect(20, 20, 370, 80);
-	public Rect windowRect1 = new Rect(20, 100, 370, 80);
+	//private
+	private Rect windowRect = new Rect(20, 20, 300, 50);
+	private bool doWindow0 = false;
+	private bool doWindow1 = false;
+	private Rect windowRect0 = new Rect(20, 20, 370, 80);
+	private Rect windowRect1 = new Rect(20, 100, 770, 80);
 	//Start making Buttons
 	void OnGUI() {
 		doWindow0 = GUI.Toggle(new Rect(10, 10, 200, 30), doWindow0, "Window 0");
-		doWindow1 = GUI.Toggle(new Rect(10, 40, 100, 20), doWindow1, "Window 1");
+		doWindow1 = GUI.Toggle(new Rect(10, 40, 400, 20), doWindow1, "Window 1");
 			if (doWindow0)
 			{
 				GUI.Window(0, new Rect(110, 10, 200, 60), DoWindow0, "Basic Window");
 				GUI.color = Color.red;
-				windowRect0 = GUI.Window(0, windowRect0, DoMyWindow, "Red Window");
+				windowRect0 = GUI.Window(0, windowRect0, DoWindow0, "Red Window");
 
 			}
 			if (doWindow1){
 				GUI.Window(1, new Rect(110, 10, 200, 90), DoWindow1, "Basic Window");
 				GUI.color = Color.green;
-				windowRect1 = GUI.Window(1, windowRect1, DoMyWindow, "Green Window");
+				windowRect1 = GUI.Window(1, windowRect1, DoWindow1, "Green Window");
 			}
 		}
 	void DoWindow0(int windowID) {
-		GUI.Button(new Rect(10, 30, 80, 20), "Do this and that!");
-	}
-	void DoWindow1(int windowID) {
-		GUI.Button(new Rect(10, 30, 80, 20), "Do that and this!");
-	}
-
-
-	void DoMyWindow(int windowID) {
 		if (GUI.Button(new Rect(10, 20, 120, 23), "Tower upgrade 1")){
-
+			
 			print("Shiny colors: " + GUI.color);
 		}
-		GUI.DragWindow(new Rect(0, 0, 10000, 10000));
-		
+	}
+	void DoWindow1(int windowID) {
+		if (GUI.Button(new Rect(10, 20, 120, 23), "Tower upgrade 1")){
+			
+			print("Shiny colors: " + GUI.color);
+		}
 	}
 }
