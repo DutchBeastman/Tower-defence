3dc7982e99f6848d231b69a84f17e4783ad83a04
jeremybond <jeremybond2@hotmail.com>
2013-12-06 12:22:01 +0100

    catching up

---
+++ Assets/Scripts/CameraBehaviourScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraBehaviourScript : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.A)
+		{
+			target Transform;
+			transform.LookAt(target);
+			transform.Translate(Vector3.right * Time.deltaTime);
+		}
+	}
+
+
+}
