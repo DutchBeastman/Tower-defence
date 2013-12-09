958f527513528ecb43ad3770b2f87476670b81be
DutchBeastman <Fabianverk@hotmail.com>
2013-12-09 22:19:52 +0100

    Spawning and some of that works not sure why

--- Assets/Scripts/ShootingTower.cs
+++ Assets/Scripts/ShootingTower.cs
@@ -1,32 +1,41 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class ShootingTower : MonoBehaviour {
 	public GameObject target;
-	public GameObject Bullet;
+	public Transform muzzle;
 	private float tijd;
+	List<GameObject> enemiesInRange = new List<GameObject>(); 
 	private bool rotating = false;
 	void Start(){
-		tijd = 1;
+		tijd = 1f;
 	}
-	void Update(){
+	void FixedUpdate(){
 		if(rotating == true)
 		{
 			tijd -= Time.deltaTime;
 			transform.LookAt(target.transform.position);
 			if(tijd <= 0)
 			{
-				GameObject newBullet = Instantiate(Resources.Load("Prefabs/Bullet"), transform.position, Quaternion.identity) as GameObject;
+				GameObject newBullet = Instantiate(Resources.Load("Prefabs/Rocket"), transform.position, Quaternion.identity) as GameObject;
+				newBullet.name = "Bullet";
+				newBullet.transform.Rotate(0, 0, 0);
+				//maakt het weird spawnen achter de building
+				//newBullet.transform.Translate(transform.position);
 
-				tijd = 1;
+				tijd = 1f;
 			}
-			//newBullet.transform.Translate (-1, 0, 0);
+
+
 		}
 	}
 	void OnTriggerEnter(Collider col)
 	{	
 		if(col.name == "Enemy"){
 			rotating = true;
+			enemiesInRange.Add(col.gameObject);
+			target = enemiesInRange[0];
 		}
 	}
 	void OnTriggerExit(Collider col)
