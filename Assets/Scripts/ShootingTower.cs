faf4b59fa9f672f59d462d7dd10db46b791f1a81
jeremybond <jeremybond2@hotmail.com>
2013-12-12 10:19:02 +0100

    pushing working version

--- Assets/Scripts/ShootingTower.cs
+++ Assets/Scripts/ShootingTower.cs
@@ -23,20 +23,17 @@ public class ShootingTower : MonoBehaviour {
 			{
 				GameObject newBullet = Instantiate(Resources.Load("Prefabs/Rocket"), transform.position, Quaternion.identity) as GameObject;
 				newBullet.name = "Bullet";
-<<<<<<< HEAD
-				float newXPos = (newBullet.transform.position.x-target.transform.position.x)*-1;
-				float newYPos = (newBullet.transform.position.y-target.transform.position.y)*-1;
-				float newZPos = (newBullet.transform.position.z-target.transform.position.z)*-1;
+				float newXPos = (newBullet.transform.position.x-target.transform.position.x) * -1;
+				float newYPos = (newBullet.transform.position.y-target.transform.position.y) * -1;
+				float newZPos = (newBullet.transform.position.z-target.transform.position.z) * -1;
 				float bSp = 10;
 				newBullet.rigidbody.AddRelativeForce(newXPos*bSp, newYPos*bSp, newZPos*bSp);
 				tijd = 0.1f;
+				///newBullet.transform.Rotate(0, 0, 0);
+				///newBullet.transform.Translate(transform.position);
+				///tijd = 0.1f;
 			}
-=======
-				newBullet.transform.Rotate(0, 0, 0);
-				newBullet.transform.Translate(transform.position);
-				//newBullet.AddRelativeForce(PositionTarget);
-				tijd = 0.1f;
-			}
+
 		}
 
 	}
@@ -44,8 +41,6 @@ public class ShootingTower : MonoBehaviour {
 	{
 		if(col.name == "Enemy"){
 			PositionTarget = new Vector3(target.transform.position.x * 100,0,target.transform.position.z * 100);
-
->>>>>>> 841fbfe1e2b720d428ecc5776b3091c3c17cbbf2
 		}
 	}
 	void OnTriggerEnter(Collider col)
@@ -59,7 +54,6 @@ public class ShootingTower : MonoBehaviour {
 	}
 	void OnTriggerExit(Collider col)
 	{
-		target = enemiesInRange[enemiesInRange.Clear()];
 		if(col.name == "Enemy")
 		{
 			rotating = false;
