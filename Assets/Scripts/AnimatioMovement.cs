cb6e936e9fe0d785e20a404222075b5026abeb9c
DutchBeastman <Fabianverk@hotmail.com>
2014-01-10 11:14:47 +0100

    Commiting an Eroor

---
+++ Assets/Scripts/AnimatioMovement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimatioMovement : MonoBehaviour {
+	public int speed = 10;
+	public int cols;
+	public int rows;
+	public int cells;
+	public bool shotfired;
+	private ShootingTower shootingtower;
+	private FiringScript sprAnim;
+
+	
+	void Start(){
+		sprAnim = GetComponent<FiringScript>();
+
+
+
+	}
+	// Update is called once per frame
+	void FixedUpdate () {
+		shotfired = GameObject.Find("Turret").GetComponent<ShootingTower>().shooting; 
+		if(shotfired == true){
+			float x =+ speed;
+			//Vector2 movement = new Vector2(x, 0);
+			//gameObject.transform.Translate(movement,0);
+			float timer = 0;
+			timer += Time.deltaTime;
+			if(timer <= 1)
+			{
+				sprAnim.Animate(cols, rows, 0, 0, cells, speed);
+				timer = 0;
+			}
+		}
+	}
+}
