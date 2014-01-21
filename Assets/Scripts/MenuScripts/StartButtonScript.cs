a715211d8b95b13ee92e27826811cb94045e3015
DutchBeastman <Fabianverk@hotmail.com>
2014-01-21 13:54:43 +0100

    111-023

--- Assets/Scripts/MenuScripts/StartButtonScript.cs
+++ Assets/Scripts/MenuScripts/StartButtonScript.cs
@@ -1,9 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(AudioSource))]
 public class StartButtonScript : MonoBehaviour {
 	public GameObject candySetter;
+	public AudioClip Intro;
+	void Start(){
+		audio.PlayOneShot(Intro, 1F);
+	}
 	void OnMouseDown(){
 		Application.LoadLevel("LVL1");
+
+
 	}
 }
